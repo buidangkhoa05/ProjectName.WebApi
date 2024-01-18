8fcdbc59e5d03208b332920d7f1156fa529003d2
khoabui <buidangkhoa05@gmail.com>
2024-01-18 09:56:09 +0700

    (+) Add Excel process

---
+++ Domain/Common/Excel/ExcelResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAid.Domain.Common.Excel
+{
+    public class ExcelResult<T>
+    {
+        public T Data { get; set; } = default(T);
+
+        public bool Status { get; set; }
+
+        public string InnerErrorMessage { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string Code { get; set; }
+    }
+}
